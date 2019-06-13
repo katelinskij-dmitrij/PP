f4a23dcae80df4e2391641b63227e34fa2c0b37d
Дмитрий <katelinskiidmitrii98@gmail.com>
2019-06-13 12:49:50 +0300

    update lw4

---
+++ lw4/lab4/Views/InputForm.cs
@@ -0,0 +1,67 @@
+﻿using lab4.Controllers;
+using lab4.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4.Views
+{
+	public partial class InputForm : Form
+	{
+		private readonly InputFormController _inputFormController;
+
+		public InputForm(DataManager data)
+		{
+			InitializeComponent();
+
+			_inputFormController = new InputFormController(data);
+			SubscribeToEvents();
+		}
+
+		private void SubscribeToEvents()
+		{
+			_inputFormController.ListCreated += ShowTimeList;
+			inputText.TextChanged += _inputFormController.OnInputFileNameChanged;
+			outputText.TextChanged += _inputFormController.OnOutputFileNameChanged;
+			iterationsText.TextChanged += _inputFormController.OnAmountOfIterationsChanged;
+			getResultButton.MouseClick += (sender, EventArgs) => { _inputFormController.OnGetResultButtonClicked(sender, EventArgs, asyncCheckBox.Checked); }; ;
+		}
+
+		public void ShowTimeList(List<long> times, bool isAsync)
+		{
+			MessageBox.Show($"Work { (isAsync ? "async" : "sync") }:\n middle: { times.Sum(time => time) / times.Count }");
+
+			timeList.Items.Clear();
+			foreach (var item in times)
+			{
+				timeList.Items.Add(item.ToString());
+			}
+		}
+
+        private void GetResultButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TimeList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
