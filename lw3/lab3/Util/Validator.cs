213e12ee3833e988056347ae930abeac5c5e16e4
Дмитрий <katelinskiidmitrii98@gmail.com>
2019-06-14 06:46:19 +0300

    update_lw4

---
+++ lw3/lab3/Util/Validator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab3.Util
+{
+    public static class Validator
+    {
+        public static int[] GetArgsIntArrFromSrtArr(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                throw new ArgumentException( "Число аргументов не менее трех" );
+            }
+
+            return new int [] { GetIntValueFromString( args [ 0 ] ), GetIntValueFromString( args [ 1 ] ), GetIntValueFromString( args [ 2 ] ) };            
+        }
+
+        private static int GetIntValueFromString(string value)
+        {
+            if (int.TryParse(value, out int result))
+            {
+                return result;
+            }
+            throw new ArgumentException( $"Значение: {value} не является числом" );
+        }
+    }
+}
